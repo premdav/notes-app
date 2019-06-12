09547b5b30af0a675bf3fa9dbdf28a47a510b353
Preston Davis <pdavis@valcom.com>
2019-06-12 15:53:25 -0500

    Added yargs to dependencies. Setting up command line options

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const notes = getNotes();
-console.log(notes);
-console.log(chalk.blue.inverse('Success'));
+// users should be able to add, remove, list, read notes
+yargs.command({
+    command: 'add',
+    describe: 'Adds a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note content',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log('Adding a new note with title:', argv.title);
+        console.log('Note contents:', argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note',
+    handler: () => {
+        console.log('Removing note');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists all notes',
+    handler: () => {
+        console.log('Listing all notes');
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a selected note',
+    handler: () => {
+        console.log('Reading note: X');
+    }
+})
+
+yargs.parse();
